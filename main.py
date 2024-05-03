3d7db07a310525a4fa2126eb77a76f72c6e45b2c
Abhinavnair13 <abhinavnair132002@gmail.com>
2024-05-03 13:29:00 +0530

    text to speech

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import requests
-import json
+import win32com.client as wincom
 
-city = input("Enter the city for which you want ot see the weather ")
-url = f"http://api.weatherapi.com/v1/current.json?key=84f6765d8ea9483fa4d70618240305&q={city}&aqi=no"
-r = requests.get(url)
 
-response_dict =json.loads(r.text)
-print(response_dict['current']['temp_c'])
+
+speak = wincom.Dispatch("SAPI.SpVoice")
+
+text =input("Enter text to speak")
+speak.Speak(text)
